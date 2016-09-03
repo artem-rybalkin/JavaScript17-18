d30933debb8508e25a6acab2c7b0c3ff5e4cf58a
Artem Rybalkin <artem.rybalkin86@gmail.com>
2016-09-03 22:33:31 +0300

    third commit

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,15 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    concat_css: {
+    options: {
+      // Task-specific options go here. 
+    },
+    all: {
+      src: ["css/src/*.css"],
+      dest: "css/dist/styles.main.css"
+      },
+    },
     concat: {
       options: {
         // define a string to put between each file in the concatenated output
@@ -20,14 +29,27 @@ module.exports = function(grunt) {
         'js/dist/script.main.min.js': ['js/dist/script.main.js']
       }
     }
+  },
+  cssmin: {
+  options: {
+    shorthandCompacting: false,
+    roundingPrecision: -1
+  },
+  target: {
+    files: {
+      'css/dist/styles.main.min.css': ['css/dist/styles.main.css']
+    }
   }
+}
   });
 
   // grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-concat-css');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-  grunt.registerTask('default', ['concat','uglify']);
+  grunt.registerTask('default', ['concat','uglify','concat_css','cssmin']);
  
 
 };
